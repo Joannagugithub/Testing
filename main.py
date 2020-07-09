db4005121ea0d94031706d4d0b9c5178c3cae59d
Nan Liu <joannagu0207@gmail.com>
2020-07-09 23:00:32 +1000

    curious about the spped of birds

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ inputted_number = input("please enter a number:")
 result_add_two=add_two(inputted_number)
 
 print(f"you entered{inputted_number}")
-print(f"the result is {result_add_two}")
+print(f"the add two result is {result_add_two}")
+
+
+def add_three(number):
+    return int(number)+3
+
+inputted_number = input("please enter a number:")
+result_add_three=add_three(inputted_number)
+
+print(f"you entered{inputted_number}")
+print(f"the add three result is {result_add_three}")
