1e487d12cf9c6e6ef76266e5ca5042cd71ee05cf
Nan Liu <joannagu0207@gmail.com>
2020-07-09 21:46:38 +1000

    first push

---
+++ main.py
@@ -0,0 +1,8 @@
+def add_two(number):
+    return int(number)+2
+
+inputted_number = input("please enter a number:")
+result_add_two=add_two(inputted_number)
+
+print(f"you entered{inputted_number}")
+print(f"the result is {result_add_two}")
